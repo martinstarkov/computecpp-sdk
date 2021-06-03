1c5ce90a21e8965fa5030cd311c8d31f2430e8ef
Martin Starkov <emailregisterlogin@gmail.com>
2021-06-03 18:38:33 +0300

    Fix conversion warnings and add instructions to title

--- demos/fluid/main.cpp
+++ demos/fluid/main.cpp
@@ -18,6 +18,8 @@ public:
 	FluidSimulationApp() : size_{ SIZE }, fluid_{ SIZE, 0.2f, 0.0f, 0.0000001f } {
 		// Set window size.
 		getWindow()->setSize({ size_ * SCALE, size_ * SCALE });
+		// Add usage instruction to application title
+		getWindow()->setTitle("Fluid Simulation - Move mouse to add fluid - Press space to clear fluid");
 	}
 
 	// Create fluid texture.
@@ -36,7 +38,9 @@ public:
 		// Check that mouse has moved.
 		if (prev_x != 0 && prev_x != 0) {
 			// Add density at mouse cursor location.
-			fluid_.AddDensity(prev_x * size_, prev_y * size_, 400, 2);
+			auto x{ static_cast<std::size_t>(prev_x * size_) };
+			auto y{ static_cast<std::size_t>(prev_y * size_) };
+			fluid_.AddDensity(x, y, 400, 2);
 		}
 		
 		// Fade overall dye levels slowly over time.
@@ -57,7 +61,7 @@ public:
 		});
 
 		// Draw texture to screen.
-		ci::Rectf screen(0, 0, getWindow()->getWidth(), getWindow()->getHeight());
+		ci::Rectf screen(0.0f, 0.0f, static_cast<float>(getWindow()->getWidth()), static_cast<float>(getWindow()->getHeight()));
 		ci::gl::draw(texture_, screen);
 	}
 private:
@@ -68,18 +72,25 @@ private:
 		}
 	}
 
+	// Dragging mouse does not include moving mouse so defined separately here.
+	void mouseDrag(ci::app::MouseEvent event) override {
+		mouseMove(event);
+	}
+
 	void mouseMove(ci::app::MouseEvent event) override {
 		// Get mouse position as fraction of window size.
-		double x{ event.getX() / double(getWindow()->getSize().x) };
-		double y{ 1.0 - event.getY() / double(getWindow()->getSize().y) };
+		auto x{ event.getX() / float(getWindow()->getSize().x) };
+		auto y{ 1.0f - event.getY() / float(getWindow()->getSize().y) };
 		// Check that previous mouse position was not only zero (for initial value to be set).
-		if (prev_x != 0 || prev_y != 0) {
+		if (prev_x != 0.0 || prev_y != 0.0) {
 			// Get amount by which mouse has moved.
 			auto amount_x{ (x - prev_x) * size_ };
 			auto amount_y{ (y - prev_y) * size_ };
 			// Add velocity and density in direction of mouse travel.
 			// Multiplied by size because x and y are in 0.0 to 1.0 relative window coordinates.
-			fluid_.AddVelocity(x * size_, y * size_, amount_x, amount_y);
+			auto current_x{ static_cast<std::size_t>(x * size_) };
+			auto current_y{ static_cast<std::size_t>(y * size_) };
+			fluid_.AddVelocity(current_x, current_y, amount_x, amount_y);
 		}
 		// Update previous mouse position.
 		prev_x = x;
@@ -87,11 +98,11 @@ private:
 	}
 	
 	// Square size of fluid container.
-	std::size_t size_{ 0 };
+	int size_{ 0 };
 	
 	// Store previous mouse positions.
-	double prev_x{ 0 };
-	double prev_y{ 0 };
+	float prev_x{ 0 };
+	float prev_y{ 0 };
 
 	// Fluid container object.
 	SYCLFluidContainer fluid_;
